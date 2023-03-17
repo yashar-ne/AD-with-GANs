df9c1a1ebc8de78904dc3b8bd0618b77babf4fc9
Yashar Hassanpour <yasharh@miltenyibiotec.de>
2023-03-17 13:57:57 +0100

    WIP: update basicGAN.py

--- src/basicGAN.py
+++ src/basicGAN.py
@@ -191,6 +191,7 @@ plt.ylabel("Loss")
 plt.legend()
 plt.show()
 
+plt.rcParams['animation.embed_limit'] = 100
 fig = plt.figure(figsize=(8, 8))
 plt.axis("off")
 ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
