aa30fc24a5118b845ffa114010b6940fdd08750e
Yashar Nehls <yashar.ne@pm.me>
2023-04-23 15:59:43 +0200

    WIP: updated discriminator model to match the one from nb

--- src/models/discriminator.py
+++ src/models/discriminator.py
@@ -4,23 +4,29 @@ import torch.nn as nn
 class Discriminator(nn.Module):
     def __init__(self, num_feature_maps, num_color_channels):
         super(Discriminator, self).__init__()
-        self.network = nn.Sequential(
-
+        self.layer1 = nn.Sequential(
             nn.Conv2d(num_color_channels, num_feature_maps, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-
+        )
+        self.layer2 = nn.Sequential(
             nn.Conv2d(num_feature_maps, num_feature_maps * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(num_feature_maps * 2),
             nn.LeakyReLU(0.2, inplace=True),
-
+        )
+        self.layer3 = nn.Sequential(
             nn.Conv2d(num_feature_maps * 2, num_feature_maps * 4, 3, 2, 1, bias=False),
             nn.BatchNorm2d(num_feature_maps * 4),
             nn.LeakyReLU(0.2, inplace=True),
-
+        )
+        self.fc = nn.Sequential(
             nn.Conv2d(num_feature_maps * 4, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
-    def forward(self, input):
-        output = self.network(input)
-        return output.view(-1, 1).squeeze(1)
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        feature = out
+        out = self.fc(out)
+        return out.view(-1, 1).squeeze(1), feature
