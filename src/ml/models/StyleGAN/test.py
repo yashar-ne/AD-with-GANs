9fee96c2de0347e5581bac4fc43889c922222491
Yashar Nehls <yashar.ha@gmail.com>
2024-01-13 18:19:13 +0100

    included stylegan rudimentary image generation

--- src/ml/models/StyleGAN/test.py
+++ src/ml/models/StyleGAN/test.py
@@ -1,61 +1,77 @@
 import os
-import re
 from typing import List, Optional
 
-import click
-import dnnlib
-import numpy as np
 import PIL.Image
 import torch
 
+import dnnlib
 import legacy
 
-def generate_image(
-    network_pkl,
-    outdir,
-    class_idx: Optional[int],
-    truncation_psi: float,
-    noise_mode: str
+device = torch.device('cuda')
+
+
+def generate_stylegan2_image(
+        network_pkl,
+        outdir,
+        class_idx: Optional[int],
+        truncation_psi: float,
+        noise_mode: str,
+        seeds: Optional[List[int]]
 ):
-    print(f"Generating Images, Outpout in {outdir}")
+    print(f"Generating Images, output in {outdir}")
     print('Loading networks from "%s"...' % network_pkl)
-    device = torch.device('cuda')
+
     with dnnlib.util.open_url(network_pkl) as f:
-        G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+        G = legacy.load_network_pkl(f)['G_ema'].to(device)  # type: ignore
+        print(G.num_ws, G.w_dim)
 
     os.makedirs(outdir, exist_ok=True)
-
+    projected_w = None
     # Synthesize the result of a W projection.
-    # if projected_w is not None:
-    #     if seeds is not None:
-    #         print ('warn: --seeds is ignored when using --projected-w')
-    #     print(f'Generating images from projected W "{projected_w}"')
-    #     ws = np.load(projected_w)['w']
-    #     ws = torch.tensor(ws, device=device) # pylint: disable=not-callable
-    #     assert ws.shape[1:] == (G.num_ws, G.w_dim)
-    #     for idx, w in enumerate(ws):
-    #         img = G.synthesis(w.unsqueeze(0), noise_mode=noise_mode)
-    #         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-    #         img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
-    #     return
+    if projected_w is not None:
+        if seeds is not None:
+            print('warn: --seeds is ignored when using --projected-w')
+        print(f'Generating images from projected W "{projected_w}"')
+        # ws = np.load(projected_w)['w']
+        # ws = torch.tensor(ws, device=device)  # pylint: disable=not-callable
+        ws = torch.randn(1, 14, 512, device=device)
+        print(ws.shape[1:])
+        assert ws.shape[1:] == (G.num_ws, G.w_dim)
+        for idx, w in enumerate(ws):
+            img = G.synthesis(w.unsqueeze(0), noise_mode=noise_mode)
+            img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+            PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').show()
+            # img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
+        return
 
     if seeds is None:
-        ctx.fail('--seeds option is required when not using --projected-w')
+        print('error: --seeds option is required when not using --projected-w')
 
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
     if G.c_dim != 0:
         if class_idx is None:
-            ctx.fail('Must specify class label with --class when using a conditional network')
+            print('error: Must specify class label with --class when using a conditional network')
         label[:, class_idx] = 1
     else:
         if class_idx is not None:
-            print ('warn: --class=lbl ignored when running on an unconditional network')
+            print('warn: --class=lbl ignored when running on an unconditional network')
 
     # Generate images.
     for seed_idx, seed in enumerate(seeds):
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
-        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        # z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        z = torch.randn(14, 512, device=device)
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').show()
+
+
+generate_stylegan2_image(
+    network_pkl='/home/yashar/git/AD-with-GANs/data/stylegan2-celebahq-256x256.pkl',
+    outdir='/home/yashar/git/AD-with-GANs/data/test_out',
+    class_idx=None,
+    truncation_psi=0.7,
+    noise_mode='const',
+    seeds=[149209]
+)
--- src/ml/models/StyleGAN/torch_utils/ops/upfirdn2d.py
+++ src/ml/models/StyleGAN/torch_utils/ops/upfirdn2d.py
@@ -10,18 +10,19 @@
 
 import os
 import warnings
+
 import numpy as np
 import torch
-import traceback
 
+from . import conv2d_gradfix
 from .. import custom_ops
 from .. import misc
-from . import conv2d_gradfix
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 _inited = False
 _plugin = None
+
 
 def _init():
     global _inited, _plugin
@@ -31,8 +32,10 @@ def _init():
         try:
             _plugin = custom_ops.get_plugin('upfirdn2d_plugin', sources=sources, extra_cuda_cflags=['--use_fast_math'])
         except:
-            warnings.warn('Failed to build CUDA kernels for upfirdn2d. Falling back to slow reference implementation. Details:\n\n' + traceback.format_exc())
+            warnings.warn('Failed to build CUDA kernels for upfirdn2d. Falling back to slow reference implementation.')
+            # warnings.warn('Failed to build CUDA kernels for upfirdn2d. Falling back to slow reference implementation. Details:\n\n' + traceback.format_exc())
     return _plugin is not None
+
 
 def _parse_scaling(scaling):
     if isinstance(scaling, int):
@@ -42,6 +45,7 @@ def _parse_scaling(scaling):
     sx, sy = scaling
     assert sx >= 1 and sy >= 1
     return sx, sy
+
 
 def _parse_padding(padding):
     if isinstance(padding, int):
@@ -53,6 +57,7 @@ def _parse_padding(padding):
         padding = [padx, padx, pady, pady]
     padx0, padx1, pady0, pady1 = padding
     return padx0, padx1, pady0, pady1
+
 
 def _get_filter_size(f):
     if f is None:
@@ -67,7 +72,8 @@ def _get_filter_size(f):
     assert fw >= 1 and fh >= 1
     return fw, fh
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def setup_filter(f, device=torch.device('cpu'), normalize=True, flip_filter=False, gain=1, separable=None):
     r"""Convenience function to setup 2D FIR filter for `upfirdn2d()`.
@@ -115,7 +121,8 @@ def setup_filter(f, device=torch.device('cpu'), normalize=True, flip_filter=Fals
     f = f.to(device=device)
     return f
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def upfirdn2d(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1, impl='cuda'):
     r"""Pad, upsample, filter, and downsample a batch of 2D images.
@@ -163,7 +170,8 @@ def upfirdn2d(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1, impl='cu
         return _upfirdn2d_cuda(up=up, down=down, padding=padding, flip_filter=flip_filter, gain=gain).apply(x, f)
     return _upfirdn2d_ref(x, f, up=up, down=down, padding=padding, flip_filter=flip_filter, gain=gain)
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 @misc.profiled_function
 def _upfirdn2d_ref(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1):
@@ -187,7 +195,7 @@ def _upfirdn2d_ref(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1):
 
     # Pad or crop.
     x = torch.nn.functional.pad(x, [max(padx0, 0), max(padx1, 0), max(pady0, 0), max(pady1, 0)])
-    x = x[:, :, max(-pady0, 0) : x.shape[2] - max(-pady1, 0), max(-padx0, 0) : x.shape[3] - max(-padx1, 0)]
+    x = x[:, :, max(-pady0, 0): x.shape[2] - max(-pady1, 0), max(-padx0, 0): x.shape[3] - max(-padx1, 0)]
 
     # Setup filter.
     f = f * (gain ** (f.ndim / 2))
@@ -207,9 +215,11 @@ def _upfirdn2d_ref(x, f, up=1, down=1, padding=0, flip_filter=False, gain=1):
     x = x[:, :, ::downy, ::downx]
     return x
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 _upfirdn2d_cuda_cache = dict()
+
 
 def _upfirdn2d_cuda(up=1, down=1, padding=0, flip_filter=False, gain=1):
     """Fast CUDA implementation of `upfirdn2d()` using custom ops.
@@ -227,7 +237,7 @@ def _upfirdn2d_cuda(up=1, down=1, padding=0, flip_filter=False, gain=1):
     # Forward op.
     class Upfirdn2dCuda(torch.autograd.Function):
         @staticmethod
-        def forward(ctx, x, f): # pylint: disable=arguments-differ
+        def forward(ctx, x, f):  # pylint: disable=arguments-differ
             assert isinstance(x, torch.Tensor) and x.ndim == 4
             if f is None:
                 f = torch.ones([1, 1], dtype=torch.float32, device=x.device)
@@ -236,14 +246,16 @@ def _upfirdn2d_cuda(up=1, down=1, padding=0, flip_filter=False, gain=1):
             if f.ndim == 2:
                 y = _plugin.upfirdn2d(y, f, upx, upy, downx, downy, padx0, padx1, pady0, pady1, flip_filter, gain)
             else:
-                y = _plugin.upfirdn2d(y, f.unsqueeze(0), upx, 1, downx, 1, padx0, padx1, 0, 0, flip_filter, np.sqrt(gain))
-                y = _plugin.upfirdn2d(y, f.unsqueeze(1), 1, upy, 1, downy, 0, 0, pady0, pady1, flip_filter, np.sqrt(gain))
+                y = _plugin.upfirdn2d(y, f.unsqueeze(0), upx, 1, downx, 1, padx0, padx1, 0, 0, flip_filter,
+                                      np.sqrt(gain))
+                y = _plugin.upfirdn2d(y, f.unsqueeze(1), 1, upy, 1, downy, 0, 0, pady0, pady1, flip_filter,
+                                      np.sqrt(gain))
             ctx.save_for_backward(f)
             ctx.x_shape = x.shape
             return y
 
         @staticmethod
-        def backward(ctx, dy): # pylint: disable=arguments-differ
+        def backward(ctx, dy):  # pylint: disable=arguments-differ
             f, = ctx.saved_tensors
             _, _, ih, iw = ctx.x_shape
             _, _, oh, ow = dy.shape
@@ -267,7 +279,8 @@ def _upfirdn2d_cuda(up=1, down=1, padding=0, flip_filter=False, gain=1):
     _upfirdn2d_cuda_cache[key] = Upfirdn2dCuda
     return Upfirdn2dCuda
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def filter2d(x, f, padding=0, flip_filter=False, gain=1, impl='cuda'):
     r"""Filter a batch of 2D images using the given 2D FIR filter.
@@ -303,7 +316,8 @@ def filter2d(x, f, padding=0, flip_filter=False, gain=1, impl='cuda'):
     ]
     return upfirdn2d(x, f, padding=p, flip_filter=flip_filter, gain=gain, impl=impl)
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def upsample2d(x, f, up=2, padding=0, flip_filter=False, gain=1, impl='cuda'):
     r"""Upsample a batch of 2D images using the given 2D FIR filter.
@@ -340,9 +354,10 @@ def upsample2d(x, f, up=2, padding=0, flip_filter=False, gain=1, impl='cuda'):
         pady0 + (fh + upy - 1) // 2,
         pady1 + (fh - upy) // 2,
     ]
-    return upfirdn2d(x, f, up=up, padding=p, flip_filter=flip_filter, gain=gain*upx*upy, impl=impl)
+    return upfirdn2d(x, f, up=up, padding=p, flip_filter=flip_filter, gain=gain * upx * upy, impl=impl)
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 
 def downsample2d(x, f, down=2, padding=0, flip_filter=False, gain=1, impl='cuda'):
     r"""Downsample a batch of 2D images using the given 2D FIR filter.
@@ -381,4 +396,4 @@ def downsample2d(x, f, down=2, padding=0, flip_filter=False, gain=1, impl='cuda'
     ]
     return upfirdn2d(x, f, down=down, padding=p, flip_filter=flip_filter, gain=gain, impl=impl)
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
